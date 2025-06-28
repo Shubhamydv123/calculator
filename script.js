711d660b7fb5803721591de95917fba2fbc117a1
Shubham Yadav <sy8122403@gmail.com>
2025-06-28 14:35:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const display = document.getElementById('display');
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+// Keyboard support
+document.addEventListener('keydown', (e) => {
+  const keys = '0123456789/*-+%.';
+  if (keys.includes(e.key)) {
+    appendValue(e.key);
+  } else if (e.key === 'Enter') {
+    e.preventDefault();
+    calculateResult();
+  } else if (e.key === 'Backspace') {
+    deleteLast();
+  } else if (e.key === 'Escape') {
+    clearDisplay();
+  }
+});
